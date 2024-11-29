89b6e200b7454574d0d1ab4890cb67cc2913c4ec
HoruSinkara <sacha-astr2011@mail.ru>
2024-11-29 17:26:40 +0300

    Initial commit

---
+++ src/main/java/com/interview/my_products_api/MyProductsApiApplication.java
@@ -0,0 +1,13 @@
+package com.interview.my_products_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProductsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProductsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/my_products_api/MyProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.my_products_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
